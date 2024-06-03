645996d6cd1a83613f909288d1376b7df0d1fbc0
andradejs <caped.2022123tads0039@aluno.ifpi.edu.br>
2024-06-03 19:20:49 -0300

    mudanças

--- functions/index.js
+++ functions/index.js
@@ -17,11 +17,11 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash')
 
-const routes = require('./routes');
+const routes = require('../routes');
 const path = require('path');
 const helmet = require('helmet');
 const csrf = require('csurf');
-const {middlewareGlobal,checkCsrfErro,criarTokenCsrf} = require('./src/middlewares/middleware');
+const {middlewareGlobal,checkCsrfErro,criarTokenCsrf} = require('../src/middlewares/middleware');
 const cookieParser = require('cookie-parser');
 
 
